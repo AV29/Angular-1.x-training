894a9e4e53ad58f9b0657ad7dc78a83cc957ff1d
Anton Vlasik <anton.vlasik@coherentsolutions.com>
2016-09-05 16:22:54 +0300

    task1. Initial

---
+++ app.js
@@ -0,0 +1,18 @@
+
+angular.module('MyApp', [])
+
+.controller('ParentController1', function ParentController1($scope){
+
+})
+
+.controller('ParentController2', function ParentController2($scope){
+
+})
+
+.controller('ChildController1', function ChildController1($scope){
+
+})
+
+.controller('ChildController2', function ChildController2($scope){
+
+});
