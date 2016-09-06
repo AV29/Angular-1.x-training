564d7e4963a6857f7519a5f9837cab0443e09170
Anton_Vlasik <snumber29@gmail.com>
2016-09-07 00:38:26 +0300

    1_5 task completed

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 
 angular.module('MyApp', []).run(function($rootScope) {
-    $rootScope.isOpened = false;
     $rootScope.openModal = function(){
-        $rootScope.isOpened = true;
+        this.isOpened = true;
     };
-    $rootScope.closeModal = function(){
-        $rootScope.isOpened = false;
+    $rootScope.doSomeMagic = function(){
+      console.log("It's a Kind of Magic!!!");
     };
-})
-    .directive('modalWindow', function(){
+});
+
+
+angular.module('MyApp').directive('modalWindow', function(){
         return {
             restrict: 'E',
             scope: {
@@ -16,29 +17,44 @@ angular.module('MyApp', []).run(function($rootScope) {
                 actionCallback: '&'
             },
             transclude: true,
-            template: '<div class="modal-wrapper" >' +
+            template: '<div class="overlay"></div>'+
+                      '<div class="modal-wrapper" >' +
                             '<div class="modal-close-trigger">' +
-                                '<a ng-click="closeModal()" class="close"></a>' +
+                                '<a ng-click="actionCallback({value: \'Message form modal!!\'})" class="close"></a>' +
                             '</div>' +
                             '<div class="modal-title">{{title}}</div>' +
                             '<div class="modal-content">' +
                                 '<ng-transclude></ng-transclude>' +
                             '</div>' +
-                      '</div>',
-            link: function (scope, element, attrs) {
-
-            }
+                      '</div>'
         }
-    })
-.controller('ParentController1', function ParentController1($scope){
+    });
+
+
+angular.module('MyApp').controller('ParentController1', function ParentController1($scope){
         $scope.title = 'Parent Controller 1 modal window';
+        $scope.isOpened = false;
+        $scope.closeModal = function(message){
+            $scope.isOpened = false;
+            console.log(message + ' Gonna be closed!');
+        };
 })
 
 .controller('ParentController2', function ParentController2($scope){
         $scope.title = 'Parent Controller 2 modal window';
+        $scope.isOpened = false;
+        $scope.closeModal = function(message){
+            $scope.isOpened = false;
+            console.log(message + ' I have been closed!');
+        };
 })
 
-.controller('ChildController1', function ChildController1($scope){
+.controller('ChildController1', function ChildController1($scope, $timeout){
         $scope.title = 'Child Controller 1 modal window';
+        $scope.isOpened = false;
+        $scope.closeModal = function(message){
+            console.log(message + ' Ambush! Will close in 3 seconds ))');
+            $timeout(function(){$scope.isOpened = false;}, 3000);
+        };
 });
 
