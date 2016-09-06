5d741267b8fd9cec8d6e2b2a2e2bf3988ded7840
Anton_Vlasik <snumber29@gmail.com>
2016-09-06 22:49:54 +0300

    buttons and modal created

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
 
-angular.module('MyApp', [])
+angular.module('MyApp', []).run(function($rootScope) {
+    $rootScope.isOpened = false;
+    $rootScope.openModal = function(){
+        $rootScope.isOpened = true;
+    };
+    $rootScope.closeModal = function(){
+        $rootScope.isOpened = false;
+    };
+})
+    .directive('modalWindow', function(){
+        return {
+            restrict: 'E',
+            scope: {
+                title: '@',
+                actionCallback: '&'
+            },
+            transclude: true,
+            template: '<div class="modal-wrapper" >' +
+                            '<div class="modal-close-trigger">' +
+                                '<a ng-click="closeModal()" class="close"></a>' +
+                            '</div>' +
+                            '<div class="modal-title">{{title}}</div>' +
+                            '<div class="modal-content">' +
+                                '<ng-transclude></ng-transclude>' +
+                            '</div>' +
+                      '</div>',
+            link: function (scope, element, attrs) {
 
+            }
+        }
+    })
 .controller('ParentController1', function ParentController1($scope){
-
+        $scope.title = 'Parent Controller 1 modal window';
 })
 
 .controller('ParentController2', function ParentController2($scope){
-
+        $scope.title = 'Parent Controller 2 modal window';
 })
 
 .controller('ChildController1', function ChildController1($scope){
-
-})
-
-.controller('ChildController2', function ChildController2($scope){
-
+        $scope.title = 'Child Controller 1 modal window';
 });
+
