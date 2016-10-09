fd4bd4a60940e0bef16f34a5993fcb2f24306bc7
Anton_Vlasik <snumber29@gmail.com>
2016-10-09 18:49:46 +0300

    no message

--- app.js
+++ app.js
@@ -1,64 +1,54 @@
 
-angular.module('MyApp', []).run(function($rootScope) {
-    $rootScope.openModal = function(){
-        this.isOpened = true;
-    };
-    $rootScope.doSomeMagic = function(){
-      console.log("It's a Kind of Magic!!!");
-    };
-});
+angular.module('MyApp', []);
 
 
-angular.module('MyApp').directive('modalWindow', function(){
+angular.module('MyApp')
+    .directive('tabControl', function(){
         return {
             restrict: 'E',
             scope: {
-                title: '@',
-                actionCallback: '&',
-                showModal: '='
             },
             transclude: true,
-            template: '<div class="overlay" ng-show="showModal"></div>'+
-                      '<div class="modal-wrapper" ng-show="showModal">' +
-                            '<div class="modal-close-trigger">' +
-                                '<a ng-click="closeModal()" class="close"></a>' +
-                            '</div>' +
-                            '<div class="modal-title">{{title}}</div>' +
-                            '<div class="modal-content">' +
-                                '<ng-transclude></ng-transclude>' +
-                            '</div>' +
-                      '</div>',
-            link: function(scope, element, attrs) {
-                scope.closeModal = function(){
-                    scope.showModal = false;
-                    scope.actionCallback({value: 'Message form modal!!'});
-                }    
+            replace: true,
+            templateUrl: 'tabControl.html',
+            bindToController: true,
+            controllerAs: 'tabControl',
+            controller: function() {
+                var self = this;
+                self.tabs = [];
+                self.register = function register(tab){
+                    self.tabs.push(tab);
+                    if(self.tabs.length === 1) {
+                        tab.active = true
+                    }
+                };
+                self.switchTab = function(selectedTab){
+                    angular.forEach(self.tabs, function (eachTab) {
+                        if(eachTab.active && (eachTab != selectedTab)) {
+                            eachTab.active = false;
+                        }
+                    });
+                    selectedTab.active = true;
+                };
+            }
+        }
+    })
+    .directive('pane', function(){
+        return {
+            restrict: 'E',
+            require: '^tabControl',
+            scope:{
+                name: '@'
+            },
+            transclude: true,
+            replace: true,
+            template: '<div class="pane-wrapper" ng-show="active" ng-transclude></div>',
+            link: function(scope, element, attrs, tabsCtrl) {
+                scope.active = false;
+                tabsCtrl.register(scope);
             }
         }
     });
 
 
-angular.module('MyApp').controller('ParentController1', function ParentController1($scope){
-        $scope.title = 'Parent Controller 1 modal window';
-        $scope.isOpened = false;
-        $scope.onModalClosed = function(message){
-            console.log(message + ' Gonna be closed!');
-        };
-})
-
-.controller('ParentController2', function ParentController2($scope){
-        $scope.title = 'Parent Controller 2 modal window';
-        $scope.isOpened = false;
-        $scope.onModalClosed = function(message){
-            console.log(message + ' I have been closed!');
-        };
-})
-
-.controller('ChildController1', function ChildController1($scope){
-        $scope.title = 'Child Controller 1 modal window';
-        $scope.isOpened = false;
-        $scope.onModalClosed = function(message){
-            console.log(message + ' Ambush! Will close in 3 seconds ))');
-        };
-});
 
