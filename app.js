1e42a66d56ada137be8909a102ceef812e48c049
Anton Vlasik <anton.vlasik@coherentsolutions.com>
2016-10-11 19:52:16 +0300

    make it better

--- app.js
+++ app.js
@@ -6,65 +6,61 @@ angular.module('MyApp').controller('OuterController',['$http', function($http){
     $http.get('panes.json').then(function(responce){
         self.panes = responce.data;
     });
-
     self.informAboutClicked = function(tab) {
         console.log(tab.name);
     }
 }]);
 
 angular.module('MyApp')
-    .directive('tabControl', function(){
-        return {
-            restrict: 'E',
-            scope: {
-            },
-            transclude: true,
-            replace: true,
-            templateUrl: 'tab-control.html',
-            bindToController: true,
-            controllerAs: 'tabControl',
-            controller: function() {
-                var self = this;
-                self.tabs = [];
-                self.register = function register(tab){
-                    self.tabs.push(tab);
-                    if(self.tabs.length === 1) {
-                        tab.active = true
-                    }
-                };
-                self.switchTab = function(selectedTab){
-                    angular.forEach(self.tabs, function (eachTab) {
-                        if(eachTab.active && (eachTab != selectedTab)) {
-                            eachTab.active = false;
-                        }
-                    });
-
-                    selectedTab.active = true;
-                };
-            }
-        }
-    })
-    .directive('pane', function(){
-        return {
-            restrict: 'E',
-            require: '^tabControl',
-            scope:{
-                name: '@',
-                onPaneActivated: '&'
-            },
-            transclude: true,
-            replace: true,
-            template: '<div class="pane-wrapper" ng-show="active" ng-transclude></div>',
-            link: function(scope, element, attrs, tabsCtrl) {
-                scope.active = false;
-                tabsCtrl.register(scope);
-                if(scope.active) {
-                    scope.onPaneActivated(scope);
+.directive('tabControl', function(){
+    return {
+        restrict: 'E',
+        scope: {
+        },
+        transclude: true,
+        replace: true,
+        templateUrl: 'tab-control.html',
+        bindToController: true,
+        controllerAs: 'tabControl',
+        controller: function() {
+            var self = this;
+            self.tabs = [];
+            self.register = function register(tab){
+                self.tabs.push(tab);
+                if(self.tabs.length === 1) {
+                    tab.active = true
                 }
+            };
+            self.switchTab = function(selectedTab){
+                angular.forEach(self.tabs, function (eachTab) {
+                    if(eachTab.active && (eachTab != selectedTab)) {
+                        eachTab.active = false;
+                    }
+                });
 
-            }
+                selectedTab.active = true;
+                selectedTab.onPaneActivated(selectedTab);
+            };
         }
-    });
+    }
+})
+.directive('pane', function(){
+    return {
+        restrict: 'E',
+        require: '^tabControl',
+        scope:{
+            name: '@',
+            onPaneActivated: '&'
+        },
+        transclude: true,
+        replace: true,
+        template: '<div class="pane-wrapper" ng-show="active" ng-transclude></div>',
+        link: function(scope, element, attrs, tabsCtrl) {
+            scope.active = false;
+            tabsCtrl.register(scope);
+        }
+    }
+});
 
 
 
