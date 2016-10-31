26b3a839dc111a36a41bf2193b5aa9c62ca8e588
Anton Vlasik <anton.vlasik@coherentsolutions.com>
2016-10-31 18:52:11 +0300

    up wards emitter added

--- app.js
+++ app.js
@@ -1,51 +1,67 @@
-
 angular.module('MyApp', []);
 
 angular.module('MyApp')
-.directive('mainInformation', function(){
-    return {
-        restrict: 'E',
-        scope: {
-        },
-        templateUrl: 'main-information.html',
-        bindToController: true,
-        controllerAs: 'mainInformation',
-        controller: ['$rootScope', function($rootScope) {
-           var self = this,
-           	   message = 'message from RootScope eventEmmiter';	
+    .directive('mainInformation', function () {
+        return {
+            restrict: 'E',
+            scope: {},
+            transclude: true,
+            templateUrl: 'main-information.html',
+            bindToController: true,
+            controllerAs: 'mainInformation',
+            controller: function ($rootScope, $interval, $scope) {
+                var self = this,
+                    message = 'message from RootScope eventEmmiter';
 
-           self.pressTheButton = function(){
-           		$rootScope.$broadcast('pressButtonEvent', message);
-           }
-        }]
-    }
-})
-.directive('informationChunk', function() {
-    return {
-        restrict: 'E',
-        scope:{
-        },
-        templateUrl: 'information-chunk.html',
-        bindToController: true,
-        controllerAs: 'informationChunk',
-         controller: ['$scope', '$interval', function($scope, $interval) {
-           var self = this,
-           	   start = null;
-           $scope.$on('pressButtonEvent', function(event, message){
+                self.pressTheButton = function () {
+                    $rootScope.$broadcast('broadCastEvent', message);
+                }
 
-           	if(start) {
-           	  $interval.cancel(start);
+                $scope.$on('emmitEvent', function(event, message) {
+                    if (self.start) {
+                        $interval.cancel(self.start);
+                    }
+                    console.log(message);
+                    self.message = message;
+
+                    self.start = $interval(function () {
+                        self.message = '';
+                    }, 2000);
+                });
             }
-           	console.log(message);
-           	self.message = message;
+        }
+    })
+    .directive('informationChunk', function () {
+        return {
+            restrict: 'E',
+            scope: {},
+            templateUrl: 'information-chunk.html',
+            bindToController: true,
+            controllerAs: 'informationChunk',
+            controller: function ($scope, $interval) {
+                var self = this,
+                    message = 'Message from below ))'
+                    start = null;
+                $scope.$on('broadCastEvent', function (event, message) {
 
-           	start = $interval(function(){self.message = '';}, 1000);
-           });
+                    if (self.start) {
+                        $interval.cancel(self.start);
+                    }
+                    console.log(message);
+                    self.message = message;
 
-           
-        }]
-    }
-});
+                    self.start = $interval(function () {
+                        self.message = '';
+                    }, 2000);
+                });
+
+                self.pressTheButton = function() {
+                  $scope.$emit('emmitEvent', message);
+                };
+
+            }
+        }
+    });
 
 
 
