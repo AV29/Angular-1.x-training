c8d433d1cfbe1ec68667095378a02f356ca6ab64
Anton_Vlasik <snumber29@gmail.com>
2016-11-06 20:35:41 +0300

    further more

--- app.js
+++ app.js
@@ -1,24 +1,48 @@
 angular.module('MyApp', [])
 
-.controller('MainController', function MainController($http) {
+.controller('MainController', function MainController($http, $timeout, $scope) {
 
     var self = this;
+
+    this.valueToSet = '';
+    this.currentValue = {};
+
     $http.get('states.json').then(function(response){
         self.states = response.data;
+        $timeout(function(){
+            $("#select").selectX({
+                onSelect: function (selectedVal) {
 
-        //$(".js-example-basic-single").select2({
-        //    placeholder: "Select a state",
-        //    allowClear: true
-        //});
+                   self.currentValue = {};
+                    $scope.$apply(function(){
+                        Object.assign(self.currentValue, selectedVal);
+                    });
+                    console.log(selectedVal);
+                }
+            });
+        }, 0);
     });
 
+    this.setValue = function(value) {
+            var $input = $('#select .input'),
+                $text = $('.trigger-text'),
+                $targetLi = $("#select ul li[value=" + value + "]");
+            // set input value
+            $input.val(value);
 
+            // change trigger text and change item class
+            $text.text($targetLi.text());
+            $targetLi.addClass("selected").siblings().removeClass("selected");
 
-    $(".js-example-basic-single").on("select2:select", function (e) { console.log("select2:select", e); }); // event trigger
+        self.currentValue = {
+                label: $targetLi.text(),
+                value: value
+            };
+    };
 
-
+    this.getValue = function() {
+        console.log(self.currentValue);
+        return self.currentValue;
+    }
 });
 
-
-//  getValue()  $('select').val();
-//  setValue()  $("select").val("WY").trigger("change");
